6d30c9e28784979aa788d7a7bca6d5bf6f40e593
Al_P <al.e.poliakow@gmail.com>
2025-01-05 15:38:07 +1100

    Fetched and displayed sunrise time for tomorrow

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,6 @@
+const weatherInfo = document.querySelector("#weatherInfo");
+const activitiesInfo = document.querySelector("#activitiesInfo");
+
 //function to fetch weather data
 const getWeatherData = async function () {
     const request = await fetch(`http://api.weatherapi.com/v1/current.json?key=935da16f3bf140a4872111650250201&q=Mallacoota&aqi=no`);
@@ -10,31 +13,11 @@ const getWeatherData = async function () {
 
 getWeatherData();
 
-//get forecast
-const getForecast = async function () {
-    const request = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=935da16f3bf140a4872111650250201&q=Mallacoota&days=5&aqi=no&alerts=no`);
-    const data = await request.json();
-    console.log(data);
-    //displayWeatherData(data);
-    //checkWind(data);
-};
-
-getForecast();
-
-//display the fetched Weather data
-const displayWeatherData = function (data) {
-    const weatherInfo = document.querySelector("#weatherInfo");
-    weatherInfo.innerHTML = `<p>${data.current.condition.text}</p>
-    <p>${data.current.temp_c} degrees</p>
-    <p>Wind: ${data.current.wind_kph} kmph</p>`;
-};
-
+//check wind and notify if paddle boarding is safe
 const checkWind = function (data){
-    console.log(data.wind);
     const windSpeed= parseInt(data.current.wind_kph);
     console.log(windSpeed);
     const paddleMessage = document.createElement("p");
-    const activitiesInfo = document.querySelector("#activitiesInfo");
     activitiesInfo.appendChild(paddleMessage);
     if (windSpeed <= 20) {
         console.log("Paddle Board");
@@ -43,5 +26,37 @@ const checkWind = function (data){
         console.log("Too Windy");
         paddleMessage.innerText = "It's too windy to paddle board!";
     }
-}
+};
+
+//display the fetched Weather data
+const displayWeatherData = function (data) {
+    weatherInfo.innerHTML = `<p>${data.current.condition.text}</p>
+    <p>${data.current.temp_c} degrees</p>
+    <p>Wind: ${data.current.wind_kph} kmph</p>`;
+};
+
+//get forecast
+const getForecast = async function () {
+    const request = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=935da16f3bf140a4872111650250201&q=Mallacoota&days=5&aqi=no&alerts=no`);
+    const forecastData = await request.json();
+    console.log(forecastData);
+    //console.log(forecastData.forecast.forecastday[1]);// gives tomorrow's forecast
+    sunriseTomorrow(forecastData);
+};
+
+getForecast();
+
+//get tomorrow's sunrise time and display information
+const sunriseTomorrow = function (forecastData) {
+    //isolate data
+    const forecastTomorrow = forecastData.forecast.forecastday[1];
+    console.log(forecastTomorrow);
+    const sunriseTomorrow = forecastTomorrow.astro.sunrise;
+    console.log(sunriseTomorrow);
+    //display information
+    const sunriseMessage = document.createElement("p");
+    activitiesInfo.appendChild(sunriseMessage);
+        sunriseMessage.innerText = `Sunrise is at ${sunriseTomorrow} tomorrow`;
+};
+
 
